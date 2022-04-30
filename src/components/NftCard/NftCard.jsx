22be0de43272ac39b04dba21cd81d4c253f3e862
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-04-30 23:16:49 +0500

    first commit

--- src/components/NftCard/NftCard.jsx
+++ src/components/NftCard/NftCard.jsx
@@ -3,7 +3,14 @@ import "./NftCard.css";
 function NftCard({ img, desc, name, poster }) {
   return (
     <div className="wrapper-character">
-      <video autoPlay muted loop src={img} poster={poster} />
+      <video
+        playsinline="playsinline"
+        autoPlay
+        muted
+        loop
+        src={img}
+        poster={poster}
+      />
       <div className="pop-div">
         <p className="pop-text">{desc}</p>
         <h2>{name}</h2>
