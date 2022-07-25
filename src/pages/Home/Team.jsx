099f520e97e06488a0227b728cd29e42681c5a1c
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-25 20:32:59 +0500

    finalised3

--- src/pages/Home/Team.jsx
+++ src/pages/Home/Team.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import team1 from "../../assets/team/team (1).png";
 import team2 from "../../assets/team/team (2).png";
-import team3 from "../../assets/team/team (3).png";
-import team4 from "../../assets/team/team (4).png";
 import team5 from "../../assets/team/team (5).png";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
@@ -47,13 +45,6 @@ function Team() {
           >
             <SplideSlide className="swiper-lazy">
               <TeamCard
-                img={team4}
-                name={"TiSci"}
-                role={"Founder and Lead Developer"}
-              />
-            </SplideSlide>
-            <SplideSlide className="swiper-lazy">
-              <TeamCard
                 img={team5}
                 name={"Atomyc"}
                 role={"Project and Community Manager"}
@@ -68,10 +59,6 @@ function Team() {
                 name={"0xTom"}
                 role={"Solidity Developer"}
               />
-            </SplideSlide>
-
-            <SplideSlide className="swiper-lazy">
-              <TeamCard img={team3} name={"David Dotan"} role={"Founder"} />
             </SplideSlide>
           </Splide>
         </div>
